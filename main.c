0c6326aedd3f035da876fef44568a16629612f3a
Antoine <antoine.montier@sfr.fr>
2022-11-21 14:58:37 +0100

    added a part of my city

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main()
     SDL_Color*palette = initialiseColors();
 
     srand(time(0));
-
+ 
     SDL_Window *w;
     SDL_Renderer *r;
 
@@ -25,30 +25,54 @@ int main()
     SDL_bool program_launched = SDL_TRUE;
     openSDL(WIDTH, HEIGHT, 0, &w, &r);
     TTF_Font *f;
-    setFont(&f, "Roboto-Black.ttf", 20);
+    setFont(&f, "Roboto-Black.ttf", VERTEX_SIZE*1.5);
 
     char *tmp = malloc(10);
     Graph g;
-    createGraph(&g, 20, 1000);
+    createGraph(&g, 200, 1000);
+    /*addArete(&g, id++, 0, 1, NO_WEIGHT);
+    addArete(&g, id++, 0, 7, NO_WEIGHT);
+    addArete(&g, id++, 2, 14, NO_WEIGHT);
+    addArete(&g, id++, 1, 9, NO_WEIGHT);
+    addArete(&g, id++, 4, 5, NO_WEIGHT);
+    addArete(&g, id++, 5, 6, NO_WEIGHT);
+    addArete(&g, id++, 7, 2, NO_WEIGHT);
+    addArete(&g, id++, 14, 4, NO_WEIGHT);
+    addArete(&g, id++, 4, 13, NO_WEIGHT);
+    addArete(&g, id++, 13, 12, NO_WEIGHT);
+    addArete(&g, id++, 12, 6, NO_WEIGHT);
+    addArete(&g, id++, 6, 11, NO_WEIGHT);
+    addArete(&g, id++, 7, 8, NO_WEIGHT);
+    addArete(&g, id++, 9, 3, NO_WEIGHT);
+    addArete(&g, id++, 9, 8, NO_WEIGHT);
+    addArete(&g, id++, 3, 10, NO_WEIGHT);
+    addArete(&g, id++, 14, 15, NO_WEIGHT);
+    addArete(&g, id++, 15, 16, NO_WEIGHT);
+    addArete(&g, id++, 16, 6, NO_WEIGHT);
+    addArete(&g, id++, 16, 5, NO_WEIGHT);
+    addArete(&g, id++, 8, 19, NO_WEIGHT);
+    addArete(&g, id++, 19, 18, NO_WEIGHT);
+    addArete(&g, id++, 19, 20, NO_WEIGHT);
+    addArete(&g, id++, 20, 21, NO_WEIGHT);
+    addArete(&g, id++, 21, 16, NO_WEIGHT);
+    addArete(&g, id++, 10, 17, NO_WEIGHT);
+    addArete(&g, id++, 10, 11, NO_WEIGHT);
+    addArete(&g, id++, 2, 8, NO_WEIGHT);
+    addArete(&g, id++, 8, 3, NO_WEIGHT);
+    addArete(&g, id++, 0, 1, NO_WEIGHT);*/
 
-    addVertex(&g, 0, 820, 530, 1);
-    addVertex(&g, 1, 870, 330, 2);
-    addVertex(&g, 2, 920, 230, 3);
-    addVertex(&g, 3, 1120, 430, 2);
 
-    addArete(&g, 0, 0, 3, 5);
-    addArete(&g, 1, 1, 0, 15);
-    addArete(&g, 2, 3, 2, 0);
-    addArete(&g, 4, 2, 0, 3);
-    addArete(&g, 5, 3, 3, 5);
-    circlePoints(&g);
+
+
+
+
+
     creatCoordinatesSystem(reader, &g);
 
     while (program_launched)
     {
 
         background(r, 255, 255, 255, WIDTH, HEIGHT);
-
         displayGraph(r, f, &g, tmp, palette);
         SDL_RenderPresent(r); // refresh the render
 
